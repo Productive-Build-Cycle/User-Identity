1108ee87915640b7e30f4e2e374a39f0b772f566
poormohammadf <poormohammadf@gmail.com>
2025-12-27 18:20:16 +0330

    Initial commit on poormohammadf branch

---
+++ IdentityServiceSolution/Identity.Core/Application/DTOs/Role/RoleRequests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Identity.Core.Application.DTOs.Role
+{
+    public record AddRoleRequest
+    {
+        public required string Name { get; init; }
+    }
+
+    public record UpdateRoleRequest
+    {
+        public required string Id { get; init; }
+        public required string Name { get; init; }
+    }
+ 
+}
---
+++ IdentityServiceSolution/Identity.Core/Application/DTOs/Role/RoleResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Identity.Core.Application.DTOs.Role
+{
+    public record RoleResponse
+    {
+        public required string Id { get; init; }
+        public required string Name { get; init; }
+    }
+}
